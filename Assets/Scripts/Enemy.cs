793ffde8946108a75a027c45ac773a9ca5671700
kkm <chachami66@gmail.com>
2022-09-29 20:11:04 +0900

    AI implementing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,61 +16,102 @@ public class Enemy : LivingEntity
         AttackBegin,
         Attacking
     }
-    
+
     private State state;
-    
+
     private NavMeshAgent agent;
     private Animator animator;
 
     public Transform attackRoot;
     public Transform eyeTransform;
-    
+
     private AudioSource audioPlayer;
     public AudioClip hitClip;
     public AudioClip deathClip;
-    
+
     private Renderer skinRenderer;
 
     public float runSpeed = 10f;
     [Range(0.01f, 2f)] public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
-    
+
     public float damage = 30f;
     public float attackRadius = 2f;
     private float attackDistance;
-    
+
     public float fieldOfView = 50f;
     public float viewDistance = 10f;
     public float patrolSpeed = 3f;
-    
-    [HideInInspector] public LivingEntity targetEntity;
+
+    public LivingEntity targetEntity;
     public LayerMask whatIsTarget;
 
 
     private RaycastHit[] hits = new RaycastHit[10];
     private List<LivingEntity> lastAttackedTargets = new List<LivingEntity>();
-    
+
     private bool hasTarget => targetEntity != null && !targetEntity.dead;
-    
+
 
 #if UNITY_EDITOR
 
     private void OnDrawGizmosSelected()
     {
+        if (attackRoot != null)
+        {
+            Gizmos.color = new Color(1f, 0f, 0f, 0.5f);
+            Gizmos.DrawSphere(attackRoot.position, attackRadius);
+        }
+
+        if (eyeTransform != null)
+        {
+            Quaternion leftEyeRotation = Quaternion.AngleAxis(-fieldOfView * 0.5f, Vector3.up);
+
+            Vector3 leftRayDirection = leftEyeRotation * transform.forward;
+
+            Handles.color = new Color(1f, 1f, 1f, 0.2f);
+            Handles.DrawSolidArc(eyeTransform.position, Vector3.up, leftRayDirection, fieldOfView, viewDistance);
+        }
+
+
+
+
+
+
+
 
     }
-    
+
 #endif
-    
+
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
 
+        audioPlayer = GetComponent<AudioSource>();
+        skinRenderer = GetComponentInChildren<Renderer>();
+
+        Vector3 attackPivot = attackRoot.position;
+        attackPivot.y = transform.position.y;
+
+        attackDistance = Vector3.Distance(transform.position, attackPivot) + attackRadius;
+
+        agent.stoppingDistance = attackDistance;
+        agent.speed = patrolSpeed;
     }
 
     public void Setup(float health, float damage,
         float runSpeed, float patrolSpeed, Color skinColor)
     {
+        this.health = health;
+        this.startingHealth = health;
+        this.damage = damage;
+        this.runSpeed = runSpeed;
+        this.patrolSpeed = patrolSpeed;
+        skinRenderer.material.color = skinColor;
 
+        agent.speed = patrolSpeed;
     }
 
     private void Start()
@@ -94,21 +135,59 @@ public class Enemy : LivingEntity
         {
             if (hasTarget)
             {
+                if (state == State.Patrol)
+                {
+                    state = State.Tracking;
+                    agent.speed = runSpeed;
+                }
                 agent.SetDestination(targetEntity.transform.position);
             }
             else
             {
+
                 if (targetEntity != null) targetEntity = null;
+
+                if (state != State.Patrol)
+                {
+                    state = State.Patrol;
+                    agent.speed = patrolSpeed;
+
+                }
+
+                if (agent.remainingDistance <= 1f)
+                {
+                    var patrolTargetPosition =
+                        Utility.GetRandomPointOnNavMesh(transform.position, 20f, NavMesh.AllAreas);
+                    agent.SetDestination(patrolTargetPosition);
+                }
+
+                Collider[] colliders = Physics.OverlapSphere(eyeTransform.position, viewDistance, whatIsTarget);
+
+                foreach (var collider in colliders)
+                {
+                    if (!IsTargetOnSight(collider.transform))
+                    {
+                        continue;
+                    }
+                    var livingEntity = collider.GetComponent<LivingEntity>();
+                    if (livingEntity != null && !livingEntity.dead)
+                    {
+                        targetEntity = livingEntity;
+                        break;
+                    }
+                }
+
+
             }
-            
+
             yield return new WaitForSeconds(0.2f);
         }
     }
-    
+
     public override bool ApplyDamage(DamageMessage damageMessage)
     {
         if (!base.ApplyDamage(damageMessage)) return false;
-        
+
         return true;
     }
 
@@ -123,22 +202,42 @@ public class Enemy : LivingEntity
     public void EnableAttack()
     {
         state = State.Attacking;
-        
+
         lastAttackedTargets.Clear();
     }
 
     public void DisableAttack()
     {
         state = State.Tracking;
-        
+
         agent.isStopped = false;
     }
 
     private bool IsTargetOnSight(Transform target)
     {
+
+
+        Vector3 direction = target.position = eyeTransform.position;
+        direction.y = eyeTransform.forward.y;
+
+        if (Vector3.Angle(direction, eyeTransform.forward) > fieldOfView * 0.5f)
+        {
+            return false;
+        }
+
+        direction = target.position - eyeTransform.position;
+
+        RaycastHit hit;
+        if (Physics.Raycast(eyeTransform.position, direction, out hit, viewDistance, whatIsTarget))
+        {
+            if (hit.transform == target)
+            {
+                return true;
+            }
+        }
         return false;
     }
-    
+
     public override void Die()
     {
 
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -18,28 +18,43 @@ public class PlayerHealth : LivingEntity
     protected override void OnEnable()
     {
         base.OnEnable();
+        UpdateUI();
+
     }
-    
+
     public override void RestoreHealth(float newHealth)
     {
         base.RestoreHealth(newHealth);
+        UpdateUI();
     }
 
     private void UpdateUI()
     {
         UIManager.Instance.UpdateHealthText(dead ? 0f : health);
     }
-    
+
     public override bool ApplyDamage(DamageMessage damageMessage)
     {
         if (!base.ApplyDamage(damageMessage)) return false;
 
-        
+        EffectManager.Instance.PlayHitEffect(damageMessage.hitPoint, damageMessage.hitNormal, transform, EffectManager.EffectType.Flesh);
+
+        playerAudioPlayer.PlayOneShot(hitClip);
+
+        UpdateUI();
+
+
         return true;
     }
-    
+
     public override void Die()
     {
         base.Die();
+
+        playerAudioPlayer.PlayOneShot(deathClip);
+
+        animator.SetTrigger("Die");
+
+        UpdateUI();
     }
 }
